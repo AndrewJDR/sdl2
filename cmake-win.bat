9196d44d36af69766766d776120f1a79104c4042
Andrew Johnson <ajohnson@draster.com>
2015-10-01 04:13:32 -0700

    Added windows build.

--- cmake-win.bat
+++ cmake-win.bat
@@ -1,6 +1,8 @@
-mkdir build
-cd build
-cmake.exe -D CMAKE_INSTALL_PREFIX="./x64" -G "Visual Studio 12 Win64" ..\
+mkdir build\win
+cd build\win
+cmake.exe -D CMAKE_INSTALL_PREFIX="../win64" -G "Visual Studio 12 Win64" -D FORCE_STATIC_VCRT:BOOL=ON -D LIBC:BOOL=ON -D SDL_TIMERS:BOOL=OFF -D SDL_AUDIO:BOOL=OFF -D SDL_SHARED:BOOL=OFF -D CMAKE_DEBUG_POSTFIX="_debug" ..\..
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Debug /p:PreferredToolArchitecture=x64 INSTALL.vcxproj && \
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=RelWithDebInfo /p:PreferredToolArchitecture=x64 INSTALL.vcxproj
-cd ..
+cd ..\..
+copy /v /y build\win\Debug\*.pdb build\win64\lib\
+copy /v /y build\win\RelWithDebInfo\*.pdb build\win64\lib\
